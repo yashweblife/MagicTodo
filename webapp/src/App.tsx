4acd4e0fb7ad17bff558f17b9d76d57031c96d5d
yashweblife <yashweblife20@gmail.com>
2024-12-14 13:34:53 -0600

    clean up app page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,149 +1,41 @@
-import AddIcon from '@mui/icons-material/Add';
-import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Button, Card, CardActions, CardContent, Grid2, IconButton, Paper, TextField, Toolbar, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+import MainAppHeader from './components/MainAppHeader';
+import NoTodosView from './components/NoTodosView';
+import TodoListsView from './components/TodoListsView';
 import useFirebase from './hooks/useFirebase';
 import AuthPage from './pages/AuthPage';
 
-type MainAppHeaderProps = {
-	setDrawerOpen: (open: boolean) => void,
-	setAddDialogOpen: (open: boolean) => void
-	username: string
+const test = [{
+	id: "1",
+	title: "Todos",
+	date: "2022-01-01",
+	todos: [
+		{
+			id: "1",
+			title: "Todo 1",
+			description: "Description 1",
+			status: false
+		},
+		{
+			id: "2",
+			title: "Todo 2",
+			description: "Description 2",
+			status: false
+		}
+	]
+},
+{
+	id: "2",
+	title: "In Progress",
+	date: "2022-01-01",
+	todos: []
 }
-function MainAppHeader({ setDrawerOpen, setAddDialogOpen, username }: MainAppHeaderProps) {
-	return (
-		<AppBar position="static">
-			<Toolbar>
-				<IconButton onClick={() => setDrawerOpen(true)} edge="start" color="inherit" sx={{ mr: 2 }} >
-					<MenuIcon />
-				</IconButton>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					MagicTodo
-				</Typography>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					Welcome {username}!
-				</Typography>
-				<IconButton color="inherit" edge="end" onClick={() => setAddDialogOpen(true)}>
-					<AddIcon />
-					<Typography>Card</Typography>
-				</IconButton>
-			</Toolbar>
-		</AppBar>
-	)
-}
-function NoTodosView() {
-	return (
-		<div style={{
-			height: "100%",
-			display: "flex",
-			justifyContent: "center",
-			alignItems: "center"
-		}}>
-			<Paper
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-					flexDirection: "column",
-					padding: "2rem"
-				}}
-			>
-				<Typography variant="h4" color='text.secondary'>
-					You have no todos
-				</Typography>
-				<Button variant='contained'>Add One</Button>
-			</Paper>
-		</div>
-	)
-}
+]
 
-type TodoItemCardProps = {
-	todo: any
-}
-function TodoItemCard({ todo }: TodoItemCardProps) {
-	return (
-		<Card variant='outlined' key={todo.id} style={{ marginBottom: "1rem" }}>
-			<CardContent>
-				<Typography variant='h6' key={todo.id}>{todo.title}</Typography>
-				<Typography variant='body1'>{todo.description}</Typography>
-			</CardContent>
-		</Card>
-	)
-}
-
-type TodoListBoxProps = {
-	list: any
-}
-function TodoListBox({ list }: TodoListBoxProps) {
-	return (
-		<Grid2 key={list.id} size={3}>
-			<Card variant='outlined'>
-				<CardContent>
-					<Typography variant='h5'>{list.title}</Typography>
-				</CardContent>
-				<CardContent>
-					{
-						list.todos.length === 0 ?
-							<Typography>No Todos</Typography> :
-							list.todos.map((todo: any) =>
-								<TodoItemCard key={todo.id} todo={todo} />
-							)
-					}
-				</CardContent>
-				<CardActions>
-					<TextField size='small' label="Add Todo" variant="outlined" fullWidth />
-				</CardActions>
-				<CardActions>
-					<Button variant='outlined'>Delete</Button>
-					<Button variant='contained'>Add Todo</Button>
-				</CardActions>
-			</Card>
-		</Grid2>
-	)
-}
-
-type TodoListsViewProps = {
-	lists: any[]
-}
-function TodoListsView({ lists }: TodoListsViewProps) {
-	return (
-		<Grid2 container spacing={2} style={{ width: "100%", padding: "2rem" }}>
-			{
-				lists.map((list: any) => <TodoListBox key={list.id} list={list} />)
-			}
-		</Grid2>
-	)
-}
 function App() {
 	const { isLoggedIn, user, handleLogout, getUserData } = useFirebase();
 	const [userDBValue, setUserDBValue] = useState<any>(null);
-	const test_list = [{
-		id: "1",
-		title: "Todos",
-		date: "2022-01-01",
-		todos: [
-			{
-				id: "1",
-				title: "Todo 1",
-				description: "Description 1",
-				status: false
-			},
-			{
-				id: "2",
-				title: "Todo 2",
-				description: "Description 2",
-				status: false
-			}
-		]
-	},
-	{
-		id: "2",
-		title: "In Progress",
-		date: "2022-01-01",
-		todos: []
-	}
-	]
-
+	const [test_list, setTestList] = useState<any>(test);
 	useEffect(() => {
 		if (user) {
 			getUserData(user.uid).then((val) => {
