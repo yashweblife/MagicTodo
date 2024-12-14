f40d2a7bacc3c3a199cddae6b35ef9d957a7288d
yashweblife <yashweblife20@gmail.com>
2024-12-14 13:35:05 -0600

    add easy component making scriipt

---
+++ webapp/dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# if user writes "c name" make a component file
+if [ "$1" = "c" ]; then
+	if [ "$2" = "" ]; then
+		echo "Missing component name"
+	else
+		mkdir "./src/components/$2"
+		touch "./src/components/$2/index.tsx"
+		echo "export default function $2() {return()}" >> "./src/components/$2/index.tsx"
+	fi
+fi
