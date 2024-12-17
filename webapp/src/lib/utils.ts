4dde19f6c20bf32b843a827300b75c78f960352a
yashweblife <yashweblife20@gmail.com>
2024-12-17 13:41:25 -0600

    add id function

--- webapp/src/lib/utils.ts
+++ webapp/src/lib/utils.ts
@@ -2,3 +2,7 @@ export function validateEmail(email: string) {
 
 }
 export function validatePassword(password: string) { }
+
+export function generateID() {
+	return Math.random().toString(36).substring(2, 9)
+}
