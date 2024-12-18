892f0db25af0b4482109cb2e8edaafa41bb5c4f8
yashweblife <yashweblife20@gmail.com>
2024-12-17 19:07:48 -0600

    new functions

--- webapp/src/store/FirebaseStore/index.tsx
+++ webapp/src/store/FirebaseStore/index.tsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { createContext, ReactNode, useState } from "react";
 
 const firebaseConfig = {
@@ -21,13 +21,77 @@ export default function FirebaseStoreProvider({ children }: { children: ReactNod
 	const auth = getAuth(app);
 	const db = getFirestore(app);
 	const [user, setUser] = useState<User | null>(auth.currentUser);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [authState, setAuthState] = useState(() =>
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
 				setUser(user);
+				setIsLoggedIn(true);
 			}
 		})
 	)
+	const handleLogin = async (email: string, password: string) => {
+		// log user in and update user+login state
+		try {
+			const user = await signInWithEmailAndPassword(auth, email, password);
+			setIsLoggedIn(true);
+			setUser(user.user);
+			return user.user
+		} catch (error) {
+			console.log(error);
+			return (error)
+		}
+	}
+	const handleLogout = async () => {
+		// log user out+update user+login state
+		try {
+			await signOut(auth);
+			setIsLoggedIn(false);
+			setUser(null);
+		} catch (error) {
+			console.log(error);
+			return (error)
+		}
+	}
+	const handleSignUp = async (email: string, password: string, fName: string, lName: string) => {
+		// create user and update user+login state
+		try {
+			const user = await createUserWithEmailAndPassword(auth, email, password);
+			await makeUserDB(user.user.uid, email, fName, lName);
+			setIsLoggedIn(true);
+			setUser(user.user);
+			return user
+		} catch (error) {
+			console.log(error);
+			return (error)
+		}
+	}
+	const makeUserDB = async (id: string, email: string, fName: string, lName: string) => {
+		// create user in db, this is an internal function for signup
+		try {
+			await setDoc(doc(db, "users", id), {
+				firstname: fName,
+				lastname: lName,
+				email,
+				todolists: []
+			})
+		}
+	}
+	const getTodoListById = async (id: string) => {
+		// return a todolist object from db	
+	}
+	const getUserLists = async () => {
+		// return an array of todolist objects
+	}
+	const addNewListToUserDB = async (title: string) => {
+		// add new list to db
+	}
+	const deleteUser = async () => {
+		// delete user
+	}
+	const deleteListById = async (id: string) => {
+		// delete list from user db and the list db
+	}
 	const output: FirebaseContextType = {
 
 	}
