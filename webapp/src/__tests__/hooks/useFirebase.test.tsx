c17e5555b3b5a6c762164199fbfc2ef63129e916
yashweblife <yashweblife20@gmail.com>
2024-12-24 15:49:38 -0800

    test statements

--- webapp/src/__tests__/hooks/useFirebase.test.tsx
+++ webapp/src/__tests__/hooks/useFirebase.test.tsx
@@ -1,7 +1,34 @@
 import { describe, it } from "vitest";
 
 describe("Firebasse Hook Tests", () => {
+    it("should login user", () => {
+
+    })
     it("Should get user data", () => {
+
+    })
+    it("Should log out user", () => {
+
+    })
+    it("should create new list", () => {
+
+    })
+    it("should delete list", () => {
+
+    })
+    it("should add todo to list", () => {
+
+    })
+    it("should delete todo from list", () => {
+
+    })
+    it("should toggle todo status", () => {
+
+    })
+    it("should edit user data", () => {
+
+    })
+    it("should delete user", () => {
 
     })
 })
